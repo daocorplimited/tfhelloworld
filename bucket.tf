08f8992ccfdfe44567c8e85e09d2c1753121fefa
Andrey Olishchuk <bobroe.web@gmail.com>
2022-12-15 16:12:11 +0100

    cleanup

--- bucket.tf
+++ bucket.tf
@@ -6,22 +6,3 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
-
-data "aws_route53_zone" "reportsdomain" {
-  name         = local.domain_zone
-  private_zone = false
-}
-
-resource "aws_route53_record" "reports" {
-  zone_id = data.aws_route53_zone.reportsdomain.zone_id
-  name    = "reports"
-  type    = "CNAME"
-  ttl     = "5"
-
-  weighted_routing_policy {
-    weight = 10
-  }
-
-  set_identifier = "reports"
-  records        = [var.reports_elb_record]
-}
